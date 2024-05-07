d91a052a085f3814d06868d9a5396d205485bcfd
alexanderdombroski <dom21013@byui.edu>
2024-05-07 13:53:13 -0600

    Completed Ponder Activity

--- mission/display.js
+++ mission/display.js
@@ -4,6 +4,6 @@
 
 function changeTheme(event) {
     const value = event.target.value;
-    document.body.className = event.target.value;
+    document.body.className = value;
     document.getElementById("logo").setAttribute("src", value == "light" ? "byui-logo_blue.webp" : "byui-logo_white.png");
 }
