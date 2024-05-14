4865cf89515f3d5eacf3578c9672a7cc2bdd76c3
alexanderdombroski <dom21013@byui.edu>
2024-05-14 12:51:06 -0600

    Finished Cool pics part 2

--- cool-pics/coolpics.js
+++ cool-pics/coolpics.js
@@ -1,7 +1,29 @@
+(function initEventListeners() {
+    const images = document.querySelectorAll("figure > img");
+    images.forEach(image => image.addEventListener('click', openViewer));
+    document.querySelector(".close-viewer").addEventListener('click', hideViewer);
+
+    document.getElementById("menuButton").addEventListener('click', toggleMenu);
+})();
+
 function toggleMenu() {
-    const nav = document.getElementById("nav")
-    nav.hidden = !nav.hidden
+    const nav = document.getElementById("nav");
+    nav.hidden = !nav.hidden;
 }
 
-let now = new Date()
-document.getElementById("year").innerText = now.getFullYear()
+function openViewer(event) {
+    const viewer = document.querySelector(".viewer");
+    const src = event.target.src;
+    const imageName = `${src.slice(0, src.lastIndexOf('-'))}-full.jpeg`
+    const image = viewer.querySelector("img");
+    image.src = imageName;
+    image.alt = "largeImage";
+    viewer.hidden = false;
+}
+function hideViewer() {
+    const viewer = document.querySelector(".viewer");
+    viewer.hidden = true;
+}
+
+let now = new Date();
+document.getElementById("year").innerText = now.getFullYear();
