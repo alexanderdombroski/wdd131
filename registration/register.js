ea5d4f42c932100e0ea24a407dd5db0d4a55eb51
alexanderdombroski <dom21013@byui.edu>
2024-06-13 13:33:23 -0600

    finished custom form submission functions

--- registration/register.js
+++ registration/register.js
@@ -1,9 +1,24 @@
+import { summaryTemplate, participantTemplate } from "./templates.js";
+
 let count = 1;
 
 (function initListeners() {
     document.getElementById("add").addEventListener("click", (_) => addParticipant(++count));
     document.getElementById("remove").addEventListener("click", (event) => removeParticipant(event, --count));
+    document.querySelector("form").addEventListener("submit", (event) => submitForm(event, count));
 })();
+
+function submitForm(event, count) {
+    event.preventDefault();
+    event.target.hidden = true;
+    const summary = document.getElementById("summary");
+    summary.hidden = false;
+    summary.innerText = summaryTemplate( // Get Name, Participant Count, and Total Fee
+        document.getElementById("adult_name").value,
+        count,
+        Array.from(document.querySelectorAll("[id^='fee']")).reduce((acc, fee) => acc + Number(fee.value), 0)
+    );
+}
 
 function addParticipant(count) {
     const participants = document.querySelector(".participants");
@@ -21,44 +36,3 @@ function removeParticipant(event, count) {
     }
     participants.querySelector(`.participant${count + 1}`).remove();
 }
-
-function participantTemplate(count) {
-	return `<section class="participant${count}">
-        <p>Participant ${count}</p>
-        <div class="item">
-            <label for="fname${count}"> First Name<span>*</span></label>
-            <input id="fname${count}" type="text" name="fname${count}" value="" required />
-        </div>
-        <div class="item activities">
-            <label for="activity${count}">Activity #<span>*</span></label>
-            <input id="activity${count}" type="text" name="activity${count}" />
-        </div>
-        <div class="item">
-            <label for="fee${count}">Fee ($)<span>*</span></label>
-            <input id="fee${count}" type="number" name="fee${count}" />
-        </div>
-        <div class="item">
-            <label for="date${count}">Desired Date <span>*</span></label>
-            <input id="date${count}" type="date" name="date${count}" />
-        </div>
-        <div class="item">
-            <p>Grade</p>
-            <select>
-                <option selected value="" disabled selected></option>
-                <option value="1">1st</option>
-                <option value="2">2nd</option>
-                <option value="3">3rd</option>
-                <option value="4">4th</option>
-                <option value="5">5th</option>
-                <option value="6">6th</option>
-                <option value="7">7th</option>
-                <option value="8">8th</option>
-                <option value="9">9th</option>
-                <option value="10">10th</option>
-                <option value="11">11th</option>
-                <option value="12">12th</option>
-            </select>
-        </div>
-    </section>`
-}
-
