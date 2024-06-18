163c159cf6a5dcc96c077f89bf4688fd04df8009
alexanderdombroski <dom21013@byui.edu>
2024-06-18 13:44:23 -0600

    added base JavaScript and image files

---
+++ recipes/scripts/main.js
@@ -0,0 +1,22 @@
+import recipes from "./recipes.mjs"
+
+
+/* 
+Recipes
+1. Author
+2. URL
+3. isBasedOn
+4. cookTime
+5. datePublished
+6. tags (list)
+7. description
+8. image
+9. recipeIngredient (lsit)
+10. name
+11. prepTime
+12. recipeInstructions
+13. recipeYield
+14. rating
+*/
+
+
