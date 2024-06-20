311dd3b99c6d5decc7c8d89932cf5cb7dfe20fe2
alexanderdombroski <dom21013@byui.edu>
2024-06-20 16:27:58 -0600

    Search bar gives results and queries rating only so far

--- recipes/scripts/main.js
+++ recipes/scripts/main.js
@@ -1,5 +1,21 @@
 import recipes from "./recipes.mjs"
 
+(function populateDatalist() {
+    const datalist = document.getElementById("recipe-list");
+    recipes.forEach(recipe => datalist.innerHTML += `<option value="${recipe.name}">`);
+})();
+
+(function initListeners() {
+    const searchBar = document.getElementById("search-input");
+    searchBar.addEventListener('change', queryRecipe);
+    searchBar.addEventListener('click', (event) => event.target.value = "");
+})();
+
+function queryRecipe(event) {
+    const recipeObject = recipes.filter(recipe => recipe.name == event.target.value)[0];
+    loadRecipe(recipeObject);
+}
+
 
 /* 
 Recipes
@@ -19,4 +35,35 @@ Recipes
 14. rating
 */
 
+function loadRecipe(recipeObject) {
+    // Clear current recipe
+    const space = document.querySelector("main");
+    space.innerHTML = "";
+    
+    // Handle incorrect input
+    if (recipeObject == undefined) {
+        space.innerHTML = "<span>no recipe match</span>"
+        return;
+    }
+
+    // Populate current recipe
+    const rating = (req) => (recipeObject.rating >= req) ? '⭐' : '☆';
+    console.log(recipeObject.rating)
+    space.innerHTML += `
+    
+
+    
+    <span
+	    class="rating"
+	    role="img"
+	    aria-label="Rating: 4 out of 5 stars"
+    >   
+	    <span aria-hidden="true" class="icon-star">${rating(1)}</span>
+	    <span aria-hidden="true" class="icon-star">${rating(2)}</span>
+	    <span aria-hidden="true" class="icon-star">${rating(3)}</span>
+	    <span aria-hidden="true" class="icon-star-empty">${rating(4)}</span>
+	    <span aria-hidden="true" class="icon-star-empty">${rating(5)}</span>
+    </span>
+    `
+}
 
